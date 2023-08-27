b1d324c40a9f1ef656205d52486af4f5595287e1
unknown <onglongpro01@gmail.com>
2023-08-27 15:10:03 +0700

    add style with scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TopNav from './components/TopNav';
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello I'm here.</h1>
---
+++ src/components/TopNav.js
@@ -0,0 +1,14 @@
+import './TopNav.scss';
+
+const TopNav = () => {
+    return (
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    );
+}
+
+export default TopNav;
