0398d839dfa7404781d31cce0ace781f2f21c1f9
unknown <onglongpro01@gmail.com>
2023-08-27 16:36:53 +0700

    first useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import TopNav from './components/TopNav';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  let [name, setName] = useState('Long');
+  let [newName, setNewName] = useState('');
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const handleName = () => {
+    setName(newName);
+    console.log(name);
+  }
+
   return (
     <div className="App">
       <TopNav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello I'm here.</h1>
+        <h1>Hello I'm {name}.</h1>
+        <input type="text" value={newName} onChange={(event) => handleNewName(event)}></input>
+        <button type="button" onClick={() => handleName()}>Click me!</button>
       </header>
     </div>
   );
